5f3b6fd23806ffd44ee9628721f8a23e764c59be
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-07-04 17:45:03 +0200

    #6 generation uuid

--- elevator-system-api/src/routes.ts
+++ elevator-system-api/src/routes.ts
@@ -3,6 +3,9 @@ import amqp from 'amqplib';
 import { config } from './config';
 import { ElevatorCreatedEvent } from './events/elevatorCreatedEvent';
 import { ElevatorUpdatedEvent } from './events/elevatorUpdatedEvent';
+import { GetElevatorStatusQuery } from './queries/GetElevatorStatusQuery';
+import { GetAllElevatorsQuery } from './queries/GetAllElevatorsQuery';
+import generateUuid from './utils/utils';
 
 export async function setupRoutes(app: Application) {
     const connection = await amqp.connect(config.rabbitmq.url);
@@ -27,4 +30,45 @@ export async function setupRoutes(app: Application) {
 
         res.status(200).send('Elevator updated');
     });
+
+    app.get('/elevator/:id/status', async (req: Request, res: Response) => {
+        const { id } = req.params;
+
+        const query = new GetElevatorStatusQuery(Number(id));
+        const correlationId = generateUuid();
+        const responseQueue = await channel.assertQueue('', { exclusive: true });
+
+        channel.sendToQueue(config.rabbitmq.queue, Buffer.from(JSON.stringify(query)), {
+            correlationId,
+            replyTo: responseQueue.queue
+        });
+
+        channel.consume(responseQueue.queue, (msg: amqp.Message | null) => {
+            if (msg?.properties.correlationId === correlationId) {
+                const status = JSON.parse(msg.content.toString());
+                res.status(200).json(status);
+                channel.ack(msg);
+            }
+        }, { noAck: false });
+    });
+
+    app.get('/elevators/status', async (_req: Request, res: Response) => {
+        const query = new GetAllElevatorsQuery();
+        const correlationId = generateUuid();
+        const responseQueue = await channel.assertQueue('', { exclusive: true });
+
+        channel.sendToQueue(config.rabbitmq.queue, Buffer.from(JSON.stringify(query)), {
+            correlationId,
+            replyTo: responseQueue.queue
+        });
+
+        channel.consume(responseQueue.queue, (msg: amqp.Message | null) => {
+            if (msg?.properties.correlationId === correlationId) {
+                const status = JSON.parse(msg.content.toString());
+                res.status(200).json(status);
+                channel.ack(msg);
+            }
+        }, { noAck: false });
+    });
 }
+
